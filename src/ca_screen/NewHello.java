50dded32a5af090943b6aa2598771bf185d3ac33
purbey <ravindrakumar925@gmail.com>
2022-08-19 16:12:46 +0530

    discard

--- src/ca_screen/NewHello.java
+++ src/ca_screen/NewHello.java
@@ -5,6 +5,7 @@ public class NewHello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("print hello");
+System.out.println("print hello3");
 	}
 
 }
