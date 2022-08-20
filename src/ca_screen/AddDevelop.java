639d2fa2d4a36c5df1377d9087aed295feeecbb0
purbey <ravindrakumar925@gmail.com>
2022-08-20 13:22:10 +0530

    commited by ravi

--- src/ca_screen/AddDevelop.java
+++ src/ca_screen/AddDevelop.java
@@ -3,7 +3,19 @@ package ca_screen;
 public class AddDevelop {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int n=6;
+		for(int k=0;k<n;k++)
+		{
+			for (int i=n-k;i>0;i--)
+			{
+				System.out.print(" ");
+			}
+			for(int j=0;j<2*k+1;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 
 	}
 
